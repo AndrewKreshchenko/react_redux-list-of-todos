f8d5325f9cd4f80e4e75e222a350d183db52a68e
AndriiKreshchenko <mail2andyk@gmail.com>
2021-08-06 12:00:09 +0300

    Updates

---
+++ precache-manifest.af45ae01c97d7b986b28e9aa4be4159a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28d584aff82fb36af0ad109c2a16145",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7b8b087cd1488c63b33",
+    "url": "./static/css/main.a19fef64.chunk.css"
+  },
+  {
+    "revision": "d19b9edfc00ff9d594b8",
+    "url": "./static/js/2.21c423fc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.21c423fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b8b087cd1488c63b33",
+    "url": "./static/js/main.d960f244.chunk.js"
+  },
+  {
+    "revision": "6e2c239f7a333241bc85",
+    "url": "./static/js/runtime-main.cb0ead06.js"
+  }
+]);
